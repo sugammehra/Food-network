e896af8901ed4b5211dadb6c224dad01b7e46730
sugam <sugam.mehra01@gmail.com>
2021-06-17 15:51:24 +0530

    update to v2

--- views/models/recipies.js
+++
@@ -1,9 +0,0 @@
-var mongoose  =require("mongoose")
-
-var RecipieSchema = new mongoose.Schema({
-    name:String,
-    image:String,
-    ingredients:String,
-    preparation:String
-});
-module.exports= mongoose.model("Recipie",RecipieSchema)
