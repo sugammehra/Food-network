2dd5c13aa3010246bd42771794ebcf745ec69c13
sugam <sugam.mehra01@gmail.com>
2021-06-17 15:25:29 +0530

    update

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const { connect } = require("mongoose");
+if(process.env.NODE_ENV!=="production"){
+  require("dotenv").config();
+}
+console.log(process.env.SECRET);
+
+// const { connect } = require("mongoose");
+// const { join } = require("path");
 
 var express       =require("express"),
     app           =express(),
     mongoose      =require("mongoose"),
     bodyParser    =require("body-parser"),
-    request = require("request"),
+    request       =require("request"),
     passport      =require("passport"),
     LocalStrategy =require("passport-local"),
-    User          =require("./views/models/User"),
-    recipie       =require("./views/models/recipies")
+    fs            =require('fs'),//new
+    flash         =require('connect-flash'),
+    path          =require("path"),
+    User          =require("./views/models/User")
+   
+    
+    
+
+    
+var recipeRoutes  =require("./routes/recipes"),
+    indexRoutes    =require("./routes/index");
 const PORT = process.env.PORT || 5000;
  //mongoose.connect("mongodb://localhost:27017/food",{useNewUrlParser:true});
 //mongoose.connect("mongodb+srv://sugam:<pwd>@cluster0.jzv7t.mongodb.net/<dbname>",{useNewUrlParser:true},function(err,body){
 //mongoose.connect("mongodb+srv://sugam:sugam@cluster0.jzv7t.mongodb.net/food?retryWrites=true&w=majority",{useNewUrlParser:true})
 mongoose.connect("mongodb://sugam:sugam@cluster0-shard-00-00.jzv7t.mongodb.net:27017,cluster0-shard-00-01.jzv7t.mongodb.net:27017,cluster0-shard-00-02.jzv7t.mongodb.net:27017/food?ssl=true&replicaSet=atlas-mbj4l5-shard-0&authSource=admin&retryWrites=true&w=majority",{useNewUrlParser:true})
-//    if(error){
-//        console.log(err);
-//    } else{
-//        console.log("database connected");
-//    }
-//});
 app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs")
+app.use(bodyParser.json());
+app.set("view engine","ejs");
+app.use(flash());
 
 
 //var options = {
@@ -48,23 +59,9 @@ app.set("view engine","ejs")
 //});
 
 
-
-
-
-
-
-
-//Midle ware
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login")
-}
-
-//=========================
+//==================================
 //Passport Configuration
-//===================================================
+//===================================
 app.use(require("express-session")({
     secret:"this will get encode and decode",
     resave:false,
@@ -80,97 +77,16 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req,res,next){
     res.locals.currentUser=req.user;
+    res.locals.error=req.flash("error");
+    res.locals.success=req.flash("success");
     next();
 })
 
+// Importing Routes
+app.use(indexRoutes);
+app.use(recipeRoutes);
 
 
-
-
-
-app.get("/",function(req,res){
-    res.render("first")
-});
-app.get("/add",isLoggedIn,function(req,res){
-    res.render("add");
-})
-app.get("/recipies",function(req,res){
-    recipie.find({},function(err,allrecip){
-        if(err){
-            console.log(err);
-        } else{
-            res.render("recipies",{recipiess:allrecip})
-        }
-    })
-})
-app.get("/recipies/:id",isLoggedIn,function(req,res){
-    recipie.findById(req.params.id,function(err,info){
-        if(err){
-            console.log(err)
-        } else {
-            res.render("info",{info:info})
-        }
-    })
-})
-app.post("/",function(req,res){
-    var name=req.body.name
-    var ingredients=req.body.ing
-    var preparation=req.body.prep
-    var img=req.body.image
-    recipie.create({
-        name:name,
-        ingredients:ingredients,
-        preparation:preparation,
-        image:img,
-    },function(err,dish){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("dish added",dish)
-        }
-    });
-   // recipie.save(function(err){
-      //  if(err){
-      //      console.log(err);
-       // } else{
-       ///    console.log("dish saved")
-        //}
-    //});
-    res.redirect("/")
-})
-     
-//=================
-//AUTH ROUTES
-//==================
-app.get("/signup",function(req,res){
-    res.render("signup")
-})
-app.post("/signup",function(req,res){
-    var newUser=new User({username:req.body.username})
-    User.register(newUser,req.body.password,function(err,body){
-        if(err){
-            console.log(err)
-            res.send("Enter All the fields")
-            res.redirect("/signup")
-        } else{
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/")
-            })
-        }
-    })
-})
-app.get("/login",function(req,res){
-    res.render("login")
-});
-app.post("/login",passport.authenticate("local",{
-    successRedirect:"/",
-    failureRedirect:"/login"
-}),function(req,res){
-}); 
-app.get("/logout",function(req,res){
-    req.logout()
-    res.redirect("/")
-})
 app.get("*",function(req,res){
     res.send("ERROR 404: PAGE NOT FOUND")
 });
